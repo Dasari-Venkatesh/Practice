2cdf2009601a4089f6d40754e2c2720518c29476
Dasari-Venkatesh <venkateshdasari021@gmail.com>
2024-06-23 19:31:34 +0530

    connection string

--- Todo-App/backend/db.js
+++ Todo-App/backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://mymongoadmin:12345@cluster0.1reeplm.mongodb.net/Todos").then(() =>{console.log("connected")})
+mongoose.connect("mongodb+srv://mymongoadmin:password@cluster0.1reeplm.mongodb.net/Todos").then(() =>{console.log("connected")})
 
 const todoSchema = mongoose.Schema({
     title: String,
