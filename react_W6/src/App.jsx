b7c8534e99a06baa431ba91d095faa6141309b7d
Dasari-Venkatesh <venkateshdasari021@gmail.com>
2024-06-25 14:56:01 +0530

    Using react memo to reduce rerender

--- react_W6/src/App.jsx
+++ react_W6/src/App.jsx
@@ -1,9 +1,17 @@
 import React,{ useState } from "react"
+import { memo } from "react"
 
 function App() {
+  const [title,setTitle] = useState("My name is ")
+  
+  function changeName() {
+    setTitle("My number is :" + Math.random())
+  }
   return (
         <div>
-            <HeaderWithButton></HeaderWithButton>
+            <button onClick = { changeName }>Click to change name</button>
+            <Header title={title}></Header>
+            <br />
             <Header title="Venkateshfa"></Header>
             <Header title="Venkateshfa"></Header>
             <Header title="Venkateshfa"></Header>
@@ -11,19 +19,10 @@ function App() {
       )
 
 }
-function Header({title}){
-  
-  return <h1>
-    {title}
-    </h1>
-  
-}
-function HeaderWithButton(){
+const Header = memo(function ({title}){
+      return <h1>
+          {title}
+        </h1>
+})
 
-  const [firstTitle,setfirstTitle] = useState("My name is Venkatesh.");
-  return <>
-      <button onClick={() =>{setfirstTitle("My number is "+Math.random())}}>Click to change name</button>
-      <Header title={firstTitle}></Header>
-    </>
-}
 export default App
