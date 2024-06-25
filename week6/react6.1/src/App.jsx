dd55a5440e08b9bcd951fa024c5a766f2b517c05
Dasari-Venkatesh <venkateshdasari021@gmail.com>
2024-06-25 14:40:31 +0530

    reduced rerenders with state in new component

--- week6/react6.1/src/App.jsx
+++ week6/react6.1/src/App.jsx
@@ -1,16 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React,{ useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-  
-    </>
-  )
-}
+        <div>
+            <HeaderWithButton></HeaderWithButton>
+            <Header title="Venkateshfa"></Header>
+            <Header title="Venkateshfa"></Header>
+            <Header title="Venkateshfa"></Header>
+          </div>
+      )
 
+}
+function Header({title}){
+  
+  return <h1>
+    {title}
+    </h1>
+  
+}
+function HeaderWithButton(){
+
+  const [firstTitle,setfirstTitle] = useState("My name is Venkatesh.");
+  return <>
+      <button onClick={() =>{setfirstTitle("My number is "+Math.random())}}>Click to change name</button>
+      <Header title={firstTitle}></Header>
+    </>
+}
 export default App
