da0efa05c43200071810eddd7e0d114d7ab179bc
Dasari-Venkatesh <venkateshdasari021@gmail.com>
2024-06-26 14:00:59 +0530

    Keys in react

--- react_W6/src/App.jsx
+++ react_W6/src/App.jsx
@@ -1,28 +1,42 @@
-import React,{ useState } from "react"
-import { memo } from "react"
+import { useState } from "react";
+
+let GLOBAL_ID = 4;
 
 function App() {
-  const [title,setTitle] = useState("My name is ")
-  
-  function changeName() {
-    setTitle("My number is :" + Math.random())
+  const [todos, setTodos ] = new useState([{
+    id : 1,
+    title : "Do Excercise",
+    description: "Need to do Yoga"
+  },
+  {
+    id : 2,
+    title : "Attend the class",
+    description: "I will complete React."
+  },
+  {
+    id : 3,
+    title : "React hooks",
+    description : "use state and memo"
   }
-  return (
-        <div>
-            <button onClick = { changeName }>Click to change name</button>
-            <Header title={title}></Header>
-            <br />
-            <Header title="Venkateshfa"></Header>
-            <Header title="Venkateshfa"></Header>
-            <Header title="Venkateshfa"></Header>
-          </div>
-      )
+  ])
 
+  return ( <>
+  {todos.map((todo) => <Todo key={todo.id} title={todo.title} description={todo.description}/>)}
+  </>)
+   
+  
 }
-const Header = memo(function ({title}){
-      return <h1>
-          {title}
-        </h1>
-})
+
+function Todo({title,description}){
+  return(<>
+    <h1>
+        {title}
+      </h1>
+      <h3>
+        { description}
+      </h3>
+    </>    
+  )
+}
 
 export default App
