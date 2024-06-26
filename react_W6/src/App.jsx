323bf14c2340de609e85f17747903a2540fe42b7
Dasari-Venkatesh <venkateshdasari021@gmail.com>
2024-06-26 14:15:35 +0530

    Passing new react component as prop to other react component

--- react_W6/src/App.jsx
+++ react_W6/src/App.jsx
@@ -1,42 +1,22 @@
-import { useState } from "react";
-
-let GLOBAL_ID = 4;
-
 function App() {
-  const [todos, setTodos ] = new useState([{
-    id : 1,
-    title : "Do Excercise",
-    description: "Need to do Yoga"
-  },
-  {
-    id : 2,
-    title : "Attend the class",
-    description: "I will complete React."
-  },
-  {
-    id : 3,
-    title : "React hooks",
-    description : "use state and memo"
-  }
-  ])
-
-  return ( <>
-  {todos.map((todo) => <Todo key={todo.id} title={todo.title} description={todo.description}/>)}
-  </>)
-   
-  
+  return <>
+    <CardWrapper innerComponent={<TextComponent/>} />
+    <CardWrapper innerComponent={<TextComponent/>} />
+    <CardWrapper innerComponent={<TextComponent/>} />
+  </>
 }
 
-function Todo({title,description}){
-  return(<>
-    <h1>
-        {title}
-      </h1>
-      <h3>
-        { description}
-      </h3>
-    </>    
-  )
+function TextComponent(){
+  return <div>
+    Venkatesh here
+  </div>
 }
+
+function CardWrapper({innerComponent}){
+    return <div style={{border: "2px solid black",padding: 100}}>
+      {innerComponent}
+    </div>
+}
+
 
 export default App
