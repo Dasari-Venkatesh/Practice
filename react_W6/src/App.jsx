efa7661257c4cf220dae42ae5f888c23aa239936
Dasari-Venkatesh <venkateshdasari021@gmail.com>
2024-07-03 10:01:14 +0530

    Update useMemo hook to reduce calc

--- react_W6/src/App.jsx
+++ react_W6/src/App.jsx
@@ -1,20 +1,19 @@
-import { useEffect, useState } from "react";
+import { useMemo, useState } from "react";
 
 function App(){
   const [inputValue, setInputValue] = useState(0);
-  const [finalValue, setfinalValue] = useState(0);
   const [counter, setCounter] = useState(0);
 
-  useEffect(() =>{
+  let sum = useMemo(() =>{
     let sum = 0;
     for(let i = 1; i<= inputValue; i++){
       sum+=i;
     }
-    setfinalValue(sum);
+    return sum
   },[inputValue])
   return<>
     <input onChange={(e) => {setInputValue(e.target.value)} } placeholder="Enter n value here "/>
-    <h4>The sum for 1 to {inputValue} is : {finalValue}.</h4>
+    <h4>The sum for 1 to {inputValue} is : {sum}.</h4>
     <button onClick={() => {setCounter(counter+1)}}>Counter</button>
     <button>{counter}</button>
     <button onClick={() => {setCounter(0)}}>Clear</button>
