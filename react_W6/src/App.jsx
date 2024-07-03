160568ade436bb07edfc37f40cff8fa0b46148b2
Dasari-Venkatesh <venkateshdasari021@gmail.com>
2024-07-03 09:31:48 +0530

    Prevented re calc with useEffect

--- react_W6/src/App.jsx
+++ react_W6/src/App.jsx
@@ -1,29 +1,23 @@
 import { useEffect, useState } from "react";
 
 function App(){
-const [id,setId] = useState(1);
-return<>
-      <button onClick={() => setId(1)}>1</button>
-      <button onClick={() => setId(2)}>2</button>
-      <button onClick={() => setId(3)}>3</button>
-      <button onClick={() => setId(4)}>4</button>
-      <Todo id = {id}></Todo>  
-  </>
-}
+  const [inputValue, setInputValue] = useState(0);
+  const [finalValue, setfinalValue] = useState(0);
+  const [counter, setCounter] = useState(0);
 
-function Todo({id}){
-  const [todo, setTodo] = useState({'title':'','description':''});
   useEffect(() =>{
-    fetch("https://sum-server.100xdevs.com/todo?id="+id)
-    .then((res) =>res.json())
-    .then((result) => {
-      setTodo(result.todo)
-    })
-  },[id])
-
-  return <>
-    <h1>{todo.title}</h1>
-    <h4>{todo.description}</h4>
+    let sum = 0;
+    for(let i = 1; i<= inputValue; i++){
+      sum+=i;
+    }
+    setfinalValue(sum);
+  },[inputValue])
+  return<>
+    <input onChange={(e) => {setInputValue(e.target.value)} } placeholder="Enter n value here "/>
+    <h4>The sum for 1 to {inputValue} is : {finalValue}.</h4>
+    <button onClick={() => {setCounter(counter+1)}}>Counter</button>
+    <button>{counter}</button>
+    <button onClick={() => {setCounter(0)}}>Clear</button>
   </>
 }
 export default App
