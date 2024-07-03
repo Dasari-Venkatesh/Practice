c22b122e1f6b6bec06f5a065a15cdf96183f028d
Dasari-Venkatesh <venkateshdasari021@gmail.com>
2024-07-03 07:16:27 +0530

    4 Buttons to 4 todos

--- react_W6/src/App.jsx
+++ react_W6/src/App.jsx
@@ -1,34 +1,29 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 
 function App(){
-  const [id,setId] = useState(1);
-  
-  return <>
-    <button onClick={() => {
-      setId(id+1);
-    }}> Next Todo</button>
-    <Todo id = {id}></Todo>
+const [id,setId] = useState(1);
+return<>
+      <button onClick={() => setId(1)}>1</button>
+      <button onClick={() => setId(2)}>2</button>
+      <button onClick={() => setId(3)}>3</button>
+      <button onClick={() => setId(4)}>4</button>
+      <Todo id = {id}></Todo>  
   </>
 }
 
 function Todo({id}){
   const [todo, setTodo] = useState({'title':'','description':''});
-
-  useEffect(() => {
-    fetch(`https://sum-server.100xdevs.com/todo?id=${ id }`).then(async (res) => {
-      const result = await res.json();
-      // console.log(result)
+  useEffect(() =>{
+    fetch("https://sum-server.100xdevs.com/todo?id="+id)
+    .then((res) =>res.json())
+    .then((result) => {
       setTodo(result.todo)
-    }).catch((error) => console.error('Error fetching the todo:', error));
+    })
   },[id])
 
-  let title = todo.title;
-  let description = todo.description;
-  return <div>
-    <h1>{title}</h1>  
-    <h3>{description}</h3>
-  </div>
-
+  return <>
+    <h1>{todo.title}</h1>
+    <h4>{todo.description}</h4>
+  </>
 }
-
 export default App
