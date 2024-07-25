a6bc83d89e537a2026b457f6fd05b3a424e00865
Dasari-Venkatesh <venkateshdasari021@gmail.com>
2024-07-25 17:23:37 +0530

    Add bank

--- paytm/backend/db.js
+++ paytm/backend/db.js
@@ -17,8 +17,22 @@ const userSchema = new mongoose.Schema({
     password: String
     
 })
+
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+const Account = mongoose.model('Account', accountSchema);
 const Users = mongoose.model('users', userSchema);
 
 module.exports = {
-    Users,userValidationShcema
+    Users, userValidationShcema, Account
 }
